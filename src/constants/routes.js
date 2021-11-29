80eb7d20cd0ed53cb427cd41d549d5ac7ca2d888
jaorsa <38358814+jaorsa@users.noreply.github.com>
2021-11-28 19:59:56 -0600

    Routes used in App.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,9 +1,15 @@
 /**
  * Routes page (it shows in the searching bar)
  */
-export const LANDING = '/';
-export const SIGN_UP = '/signup'; 
-export const SIGN_IN = '/signin';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
+export const LANDING = "/";
+export const SIGN_UP = "/signup";
+export const SIGN_IN = "/signin";
+export const HOME = "/home";
+export const ACCOUNT = "/account";
+export const ADMIN = "/admin";
+export const RUNS = "/runs";
+export const PROD = "/products";
+export const STATIONS = "/stations";
+export const KANBANS = "/kanbans";
+export const REPORTS = "/reports";
+export const DATA = "/data";
